06459893b0d16e93729a7a1b1a56381ee97304c5
AbdulKhalekG <adnan_akg_1401@hotmail.com>
2022-08-04 01:58:17 -0400

    equisdeeeeeee

--- src/database.js
+++ src/database.js
@@ -20,7 +20,7 @@ const createuser= async (req,res) =>{
 
     const{ nombre, username, correo,clave} = req.body;
     const passwordencriptado = await helpers.encryptPassword(clave)
-    const response = await pool.query('INSERT INTO usuarios (nombre, username, correo,clave) VALUES($1, $2, $3, $4, $5, $6, $7)', [
+    const response = await pool.query('INSERT INTO usuarios (nombre, username, correo,clave) VALUES($1, $2, $3, $4)', [
         nombre, username, correo,passwordencriptado])
     console.log(response);
     res.json(response.rows)
