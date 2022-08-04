b56a46ee97108e4a2f50d5c6010763d8833fcc6d
AbdulKhalekG <adnan_akg_1401@hotmail.com>
2022-08-04 02:12:35 -0400

    modificacion

--- src/strategies.js
+++ src/strategies.js
@@ -4,10 +4,14 @@ const { Pool } = require('pg');
 const helpers =require('./helpers')
 
 const config={
-  connectionString: process.env.DATABASE_URL,
+  connectionStreing: process.env.DATABASE_URL,
   max:500,
   min:100,
-  ssl:{rejectUnauthorized:false}
+  ssl: {rejectUnauthorized:false}
+  //user:'faefrtbvusiifx',
+  //host:'ec2-54-152-28-9.compute-1.amazonaws.com',
+  //password:'5183a7d5b0f78a919654a538803702f9df706a162dad8a790cf5a5cb9796490c',
+  //database:'d54loslgcri427'
 };
   
   const pool = new Pool(config); 
@@ -27,24 +31,26 @@ const config={
       const result= await pool.query('SELECT* FROM usuarios WHERE username=$1',[user.username])
       if(result.rows.length>0){
          const newuser =result.rows[0];
+       
+         console.log(newuser.clave);
          const validpassword= await helpers.compararclave(password,newuser.clave) 
         
          if(validpassword){
-          
-          done(null,newuser,
-            res.json('welcome'))
+          //password===newuser.clave
+          done(null,newuser,console.log('welcome'))
           user.id=newuser.id_usuario
+         
           passport.serializeUser((user,done)=>{
             done(null,user.id)
           })
 
 
          }else{
-              done(null,false,res.json('password incorrect'))
+              done(null,false,console.log('password incorrect'))
               
          }
       }else{
-        return done(null, false,res.json('user no exist'))   
+        return done(null, false,console.log('user does not exist'))   
         
       }
       
